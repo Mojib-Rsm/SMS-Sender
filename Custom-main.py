04b8e82a57f531d4707e5a2fab60679112b67693
MoJiB Rsm <85434839+Mojib-Rsm@users.noreply.github.com>
2023-09-14 13:43:12 +0600

    Create Custom-main.py

---
+++ Custom-main.py
@@ -0,0 +1,21 @@
+import urllib.parse
+import requests 
+
+number = input("Enter Number":)  # Replace with the actual mobile number
+message = input("Enter Massage")# Replace with the actual message
+
+base_url = "https://mojibrsm.com/sms-api/sms-api.php?key=FREE"
+params = {
+    "num": number,
+    "msg": message
+}
+api_url = base_url + "?" + urllib.parse.urlencode(params)
+
+# Make the API request
+response = requests.get(api_url)
+
+# Handle the API response
+if response.status_code == 200:
+    print("SMS sent successfully.")
+else:
+    print("Failed to send the SMS.")
